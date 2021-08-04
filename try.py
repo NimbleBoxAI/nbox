155bd4060ae5928fb0e99942946937f77d25a1eb
Aakash kaushik <kaushikaakash7539@gmail.com>
2021-08-04 12:05:01 +0530

    added comments

--- try.py
+++ try.py
@@ -1,5 +1,9 @@
 import nbox
 
+# As all these models come from the popular frameworks you use on daily basis
+# such as torchvision or efficient_pytorch they have same parameters you can pass to the load function 
 model = nbox.load("mobilenetv2")
+# If you want to use the pre trained version
+# model = nbox.load("mobilenetv2". pretrained=True)
 out = model('./tests/assets/cat.jpg')
 print(out)
