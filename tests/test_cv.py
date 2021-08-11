b4e9f0f8bbeff976e090a9609adb5dd90727f16f
yashbonde <bonde.yash97@gmail.com>
2021-08-12 00:07:26 +0530

    add reproducibility tests

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,21 +1,30 @@
+import os
 import unittest
-from torchvision import models
 
 import nbox
 from nbox import utils
+
+# we check forward pass works and that results are reproducible
+URL_CAT_TARGET_LIST = [862, 644, 626, 470, 827]
+ASSET_CAT_TARGET_LIST = [862, 626, 470, 644, 556]
 
 class ImportComputerVision(unittest.TestCase):
 
     def test_mobilenetv2_url(self):
         model = nbox.load("torchvision/mobilenetv2")
+        model.eval()
         image = utils.get_image("https://media.newyorker.com/photos/590971712179605b11ad7a88/16:9/w_1999,h_1124,c_limit/Jabr-AreCatsDomesticated.jpg")
         out = model(image)
+        pred = out[0].topk(5).indices.tolist()
+        self.assertEqual(pred, URL_CAT_TARGET_LIST)
 
     def test_mobilenetv2_filepath(self):
-        import os
         model = nbox.load("torchvision/mobilenetv2")
+        model.eval()
         image = os.path.join(utils.folder(__file__), "assets/cat.jpg")
         out = model(image)
+        pred = out[0].topk(5).indices.tolist()
+        self.assertEqual(pred, ASSET_CAT_TARGET_LIST)
 
     def test_mobilenetv2_numpy(self):
         import numpy as np
