f1fb989d78feff26302f5fa00c35bc96385d7969
yashbonde <bonde.yash97@gmail.com>
2021-08-25 02:31:43 +0530

    consistency in UI template

--- assets/ocd.py
+++ assets/ocd.py
@@ -15,35 +15,35 @@ st = time()
 # Step 1: Obtain the access token
 with console.status("", spinner = "moon") as status:
   for _ in range(2):
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Getting access tokens ...[/bold dark_cyan]")
+    status.update(f"[bold dark_cyan]Getting access tokens ...[/bold dark_cyan]")
     sleep(1)
 console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Access token obtained")
 
 # Step 2: Converting torch model to ONNX
 with console.status("", spinner = "moon") as status:
   for _ in range(3):
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Converting torch model to ONNX ...[/bold dark_cyan]")
+    status.update(f"[bold dark_cyan]Converting torch model to ONNX ...[/bold dark_cyan]")
     sleep(1)
 console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] ONNX model conversion complete")
 
 # Step 3: Get one-time-url
 with console.status("", spinner = "moon") as status:
   for _ in range(2):
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Getting one-time-url ...[/bold dark_cyan]")
+    status.update(f"[bold dark_cyan]Getting one-time-url ...[/bold dark_cyan]")
     sleep(1)
 console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] One-time-url obtained")
 
 # Step 4: Upload file to s3
 with console.status("", spinner = "moon") as status:
   for _ in range(3):
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Uploading file to s3 ...[/bold dark_cyan]")
+    status.update(f"[bold dark_cyan]Uploading file to s3 ...[/bold dark_cyan]")
     sleep(1)
 console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] File uploaded to s3")
 
 # Step 5: Updating Web-Server about the status of the upload
 with console.status("", spinner = "moon") as status:
   for _ in range(2):
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Updating Web-Server about the status of the upload[/bold dark_cyan]")
+    status.update(f"[bold dark_cyan]Updating Web-Server about the status of the upload[/bold dark_cyan]")
     sleep(1)
 console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Web-Server informed")
 
