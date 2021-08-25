83443c9eb23cb34cfd6362a8d3c476be9643576a
yashbonde <bonde.yash97@gmail.com>
2021-08-25 16:14:12 +0530

    add requirements + detach theme in UI sim

--- assets/ocd.py
+++ assets/ocd.py
@@ -6,61 +6,69 @@ from time import sleep, time
 from datetime import timedelta
 from rich.console import Console
 
+class T:
+  clk = "deep_sky_blue1"   # timer
+  st = "bold dark_cyan"    # status + print
+  fail = "bold red"        # fail
+  inp = "code bold yellow" # in-progress
+  nbx = "bold grey0"       # text with NBX at top and bottom
+  rule = "dark_cyan"       # ruler at top and bottom
+
 # looking at all the colors might be annoying, but it is simplest way to debug and ensure
 # code is working as expected
 console = Console()
 
 # the process is step by step so perform sleep tasks inplace of .ocd() tasks
-console.rule("[bold grey0]NBX-OCD[/bold grey0]", style = "dark_cyan")
+console.rule(f"[{T.nbx}]NBX-OCD[/{T.nbx}]", style = T.rule)
 st = time()
 
 # Step 1: Obtain the access token
 with console.status("", spinner = "moon") as status:
-  status.update(f"[bold dark_cyan]Getting access tokens ...[/bold dark_cyan]")
+  status.update(f"[{T.st}]Getting access tokens ...[/{T.st}]")
   sleep(2)
-console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Access token obtained")
+console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] Access token obtained")
 
 # Step 2: Converting torch model to ONNX
 with console.status("", spinner = "moon") as status:
-  status.update(f"[bold dark_cyan]Converting torch model to ONNX ...[/bold dark_cyan]")
+  status.update(f"[{T.st}]Converting torch model to ONNX ...[/{T.st}]")
   sleep(3)
-console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] ONNX model conversion complete")
+console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] ONNX model conversion complete")
 
 # Step 3: Get one-time-url
 with console.status("", spinner = "moon") as status:
-  status.update(f"[bold dark_cyan]Getting one-time-url ...[/bold dark_cyan]")
+  status.update(f"[{T.st}]Getting one-time-url ...[/{T.st}]")
   sleep(2)
-console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] One-time-url obtained")
+console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] One-time-url obtained")
 
 # Step 4: Upload file to s3
 with console.status("", spinner = "moon") as status:
-  status.update(f"[bold dark_cyan]Uploading file to s3 ...[/bold dark_cyan]")
+  status.update(f"[{T.st}]Uploading file to s3 ...[/{T.st}]")
   sleep(3)
-console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] File uploaded to s3")
+console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] File uploaded to s3")
 
 # Step 5: Updating Web-Server about the status of the upload
 with console.status("", spinner = "moon") as status:
-  status.update(f"[bold dark_cyan]Updating Web-Server about the status of the upload[/bold dark_cyan]")
+  status.update(f"[{T.st}]Updating Web-Server about the status of the upload[/{T.st}]")
   sleep(2)
-console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Web-Server informed")
+console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] Web-Server informed")
 
 # Step 6: Polling while the model is being deployed
-with console.status("[bold dark_cyan]Polling while the model is being deployed ...[/bold dark_cyan]", spinner = "moon") as status:
-  status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Current Status: [bold yellow][code]conversion.in-progress")
+with console.status("[{T.st}]Polling while the model is being deployed ...[/{T.st}]", spinner = "moon") as status:
+  status.update(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] Current Status: [{T.inp}]conversion.in-progress")
   sleep(3)
   if random.random() > 0.0:
-    console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Conversion successful")
+    console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] [{T.st}]Conversion successful")
     sleep(1)
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Current Status: [bold yellow][code]deployment.in-progress")
+    status.update(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] Current Status: [{T.inp}]deployment.in-progress")
     sleep(3)
     if random.random() > 0.0:
-      console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold dark_cyan]Model is deployed at URL:")
-      console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]]     https://depl.nbx.ai/yashbonde/its-britney-bish")
+      console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] [{T.st}]Model is deployed at URL:")
+      console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]]     https://depl.nbx.ai/yashbonde/its-britney-bish")
     else:
-      status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Current Status: [bold yellow][code]deployment.failed")
-      console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold red]Model deployment failed")
+      status.update(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] Current Status: [{T.fail}][code]deployment.failed")
+      console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] [{T.fail}]Model deployment failed")
   else:
-    status.update(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] Current Status: [bold red][code]conversion.failed")
-    console.print(f"[[deep_sky_blue1]{str(timedelta(seconds=int(time() - st)))[2:]}[/deep_sky_blue1]] [bold red]Model conversion failed")
+    status.update(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] Current Status: [{T.fail}][code]conversion.failed")
+    console.print(f"[[{T.clk}]{str(timedelta(seconds=int(time() - st)))[2:]}[/{T.clk}]] [{T.fail}]Model conversion failed")
 
-console.rule("[bold grey0]NBX-OCD Complete[/bold grey0]", style = "dark_cyan")
+console.rule(f"[{T.nbx}]NBX-OCD Complete[/{T.nbx}]", style = T.rule)
