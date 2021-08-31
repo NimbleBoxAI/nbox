019ec298646a438d070a5022c674a3e81d88ed44
yashbonde <bonde.yash97@gmail.com>
2021-09-01 01:15:02 +0530

    need better way to hide secrets

--- assets/deploy.py
+++ assets/deploy.py
@@ -6,6 +6,6 @@ model = nbox.load("torchvision/resnet18")  # , cache_dir=cache_dir)
 
 url = model.deploy(
     input_object="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg",
-    username="rohan.pooniwala@gmail.com",
-    password="Test@01",
+    username="",
+    password="",
 )
