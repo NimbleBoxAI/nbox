b9f171a8d09d438dc597a49aaac2ec0157694a4d
yashbonde <bonde.yash97@gmail.com>
2022-02-01 01:03:47 +0530

    patch update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, nimblebox.ai"
 author = "nimblebox.ai"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.5"
+release = "0.8.6"
 
 
 # -- General configuration ---------------------------------------------------
