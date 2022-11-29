9d83e9e8df48d0cd931f867a6678ab913affa471
yashbonde <bonde.yash97@gmail.com>
2022-11-29 16:49:24 +0530

    bump 0.11.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021-22, nimblebox.ai"
 author = "research@nimblebox.ai"
 
 # The full version, including alpha/beta/rc tags
-release = "0.11.0c0"
+release = "0.11.1c13"
 
 
 # -- General configuration ---------------------------------------------------
